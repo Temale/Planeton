51887235128f5713df66197363f3bfd3c8873195
Catie Jones <catiejones@Caties-MacBook-Pro.local>
2017-04-25 23:03:36 -0700

    Music Attempt
    Added music notes and tried to do code but i do not do code good

---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,26 @@
+﻿//using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour {
+
+	public AudioClip SoundToPlay;
+	public float Volume;
+	AudioSource audio;
+	public bool alreadyPlayed = false;
+	void Start()
+	{
+		audio = GetComponent<AudioSource>();
+	}
+
+	void OnTriggerEnter(Collider otherObj)
+	{
+		if (otherObj.tag == "Player")
+		{
+			print ("played note");
+			audio.PlayOneShot(SoundToPlay, Volume);
+			alreadyPlayed = true;
+		}
+	}
+}
+
